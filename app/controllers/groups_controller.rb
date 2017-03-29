276cc62ad80068c029b7035e76b8ac0eaca536b6
lyzgg5 <lyzgg5@gmail.com>
2017-03-29 21:52:07 +0800

    create創造新討論及儲存設定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,6 +4,19 @@ class GroupsController < ApplicationController
   end
 
   def new
-    @group =Group.new    
+    @group =Group.new
+  end
+
+def create
+  @group =Group.new(group_params)
+  @group.save
+
+  redirect_to group_path
+end
+
+
+private#私自定義
+  def group_params
+    params.require(:group).permit(:title, :description)
   end
 end
