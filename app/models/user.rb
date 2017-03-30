00696f65ecec3fa0f452212070576bb7abde96f5
lyzgg5 <lyzgg5@gmail.com>
2017-03-30 23:21:59 +0800

    建立group relationship及group、user間的連結

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
   has_many :groups
+
+  has_many :groups_relationships
+  has_many :participated_groups, :through ==> :groups_relationships, :source => :group
 end
